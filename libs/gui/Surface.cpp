ce9717e54af91d1582247bde8071992bcd5521c5
Ramkumar Radhakrishnan <ramkumar@codeaurora.org>
2012-11-07 11:30:19 -0800

    Add support for custom buffer sizes.
    Add native window properties NATIVE_WINDOW_SET_BUFFERS_SIZE to the
    perform function of Surface to set the user defined size
    of graphic buffers. This is needed for adding support for
    the playback of interlaced content.
    
    Change-Id: I8354c29d2ffcc03178fae86dfa449cbefd24ae55

--- libs/gui/Surface.cpp
+++ libs/gui/Surface.cpp
@@ -67,6 +67,7 @@ Surface::Surface(
     mReqHeight = 0;
     mReqFormat = 0;
     mReqUsage = 0;
+    mReqSize = 0;
     mTimestamp = NATIVE_WINDOW_TIMESTAMP_AUTO;
     mCrop.clear();
 #ifdef QCOM_BSP
@@ -468,6 +469,9 @@ int Surface::perform(int operation, va_list args)
     case NATIVE_WINDOW_SET_BUFFERS_FORMAT:
         res = dispatchSetBuffersFormat(args);
         break;
+    case NATIVE_WINDOW_SET_BUFFERS_SIZE:
+        res = dispatchSetBuffersSize(args);
+        break;
     case NATIVE_WINDOW_LOCK:
         res = dispatchLock(args);
         break;
@@ -546,6 +550,11 @@ int Surface::dispatchSetBuffersFormat(va_list args) {
     return setBuffersFormat(f);
 }
 
+int Surface::dispatchSetBuffersSize(va_list args) {
+    int size = va_arg(args, int);
+    return setBuffersSize(size);
+}
+
 int Surface::dispatchSetScalingMode(va_list args) {
     int m = va_arg(args, int);
     return setScalingMode(m);
@@ -621,6 +630,7 @@ int Surface::disconnect(int api) {
         mReqWidth = 0;
         mReqHeight = 0;
         mReqUsage = 0;
+        mReqSize = 0;
         mCrop.clear();
         mScalingMode = NATIVE_WINDOW_SCALING_MODE_FREEZE;
         mTransform = 0;
@@ -720,6 +730,22 @@ int Surface::setBuffersFormat(int format)
 
     Mutex::Autolock lock(mMutex);
     mReqFormat = format;
+    return NO_ERROR;
+}
+
+int Surface::setBuffersSize(int size)
+{
+    ATRACE_CALL();
+    ALOGV("Surface::setBuffersSize");
+
+    if (size<0)
+        return BAD_VALUE;
+
+    Mutex::Autolock lock(mMutex);
+    if(mReqSize != (uint32_t)size) {
+        mReqSize = size;
+        mGraphicBufferProducer->setBuffersSize(size);
+    }
     return NO_ERROR;
 }
 
