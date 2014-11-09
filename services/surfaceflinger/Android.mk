5db208408a7e9099958920cba3fa5e5de4e12938
Ethan Chen <intervigil@gmail.com>
2014-11-09 00:09:14 -0800

    frameworks/native: Use project pathmap
    Change-Id: I29498dbc72094e48c871768675708e96b2f10556

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -141,8 +141,8 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
     LOCAL_C_INCLUDES += $(BOARD_DISPLAY_HAL)/libgralloc
     LOCAL_C_INCLUDES += $(BOARD_DISPLAY_HAL)/libqdutils
   else
-    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
-    LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libqdutils
   endif
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_CFLAGS += -DQTI_BSP
