77bbc198304158904c2a372e67963271f09838e4
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-11-17 23:35:32 -0800

    Revert "get rid of MemoryHeapPmem"
    This reverts commit 7a7524810a0950c1c2b18014a8f1c637275165c4 and
    makes MemoryHeapPmem conditioned to BOARD_NEEDS_MEMORYHEAPPMEM
    
    Change-Id: I2f38340984ba87d6970d17ec944e3faded4132c8

--- libs/binder/Android.mk
+++ libs/binder/Android.mk
@@ -42,6 +42,11 @@ sources := \
     Status.cpp \
     TextOutput.cpp \
 
+ifeq ($(BOARD_NEEDS_MEMORYHEAPPMEM),true)
+sources += \
+    MemoryHeapPmem.cpp
+endif
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -51,6 +56,12 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
 LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
 LOCAL_SRC_FILES := $(sources)
+ifeq ($(BOARD_NEEDS_MEMORYHEAPPMEM),true)
+LOCAL_C_INCLUDES += \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+endif
 ifneq ($(TARGET_USES_64_BIT_BINDER),true)
 ifneq ($(TARGET_IS_64_BIT),true)
 LOCAL_CFLAGS += -DBINDER_IPC_32BIT=1
@@ -63,6 +74,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libbinder
 LOCAL_STATIC_LIBRARIES += libutils
 LOCAL_SRC_FILES := $(sources)
+ifeq ($(BOARD_NEEDS_MEMORYHEAPPMEM),true)
+LOCAL_C_INCLUDES += \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+endif
 ifneq ($(TARGET_USES_64_BIT_BINDER),true)
 ifneq ($(TARGET_IS_64_BIT),true)
 LOCAL_CFLAGS += -DBINDER_IPC_32BIT=1
