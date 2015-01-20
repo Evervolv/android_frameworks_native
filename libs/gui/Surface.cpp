06fbcf004bbba099d27fafad74b4e7aa665ca86c
Christopher N. Hesse <raymanfx@gmail.com>
2015-01-20 18:54:54 +0100

    libgui: add support for Samsung external gralloc usecases
    Taken from insignal:
    http://git.insignal.co.kr/insignal/arndale_octa-jb_mr1.1/android/platform/frameworks/native/commit/?h=arndale_octa-jb_mr1.1&id=4695f276a4dd642056653eca1431b0b9137088bf
    
    Change-Id: I3113304bb92d076b723b98185674759df487a93a

--- libs/gui/Surface.cpp
+++ libs/gui/Surface.cpp
@@ -807,6 +807,14 @@ status_t Surface::lock(
             return err;
         }
         // we're intending to do software rendering from this point
+#ifdef SAMSUNG_GRALLOC_EXTERNAL_USECASES
+        if(!(mReqUsage & GRALLOC_USAGE_EXTERNAL_DISP) &&
+                !(mReqUsage & GRALLOC_USAGE_EXTERNAL_ONLY) &&
+                !(mReqUsage & GRALLOC_USAGE_EXTERNAL_BLOCK) &&
+                !(mReqUsage & GRALLOC_USAGE_EXTERNAL_FLEXIBLE) &&
+                !(mReqUsage & GRALLOC_USAGE_EXTERNAL_VIRTUALFB) &&
+                !(mReqUsage & GRALLOC_USAGE_INTERNAL_ONLY))
+#endif
         setUsage(GRALLOC_USAGE_SW_READ_OFTEN | GRALLOC_USAGE_SW_WRITE_OFTEN);
     }
 
